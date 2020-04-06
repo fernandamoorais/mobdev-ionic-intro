fd17e996d795ec17e5d5318d0ef31c702a4f98b1
fernandamoorais <fernandamoorais@hotmail.com>
2020-04-06 18:57:28 +0000

    week4-1

--- mobdev-ionic-intro/src/app/app.module.ts
+++ mobdev-ionic-intro/src/app/app.module.ts
@@ -1,11 +1,15 @@
-import {
-NgModule
-} from '@angular/core';
-import {
-PreloadAllModules,
-RouterModule,
-Routes
-} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules,RouterModule, RouteReuseStrategy,Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+ 
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+ 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [{
 path: '',
 loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
@@ -19,4 +23,16 @@ preloadingStrategy: PreloadAllModules
 exports: [RouterModule]
 })
 export class AppRoutingModule {}
-12
+	
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
